f323d84f447c5c72bfc4da3e8cb5b1860ef5d8b8
34-43 <121276581+34-43@users.noreply.github.com>
2024-01-31 02:45:00 +0900

    feat: chart now shows pal image

--- api/static/js/main.js
+++ api/static/js/main.js
@@ -14,18 +14,25 @@ $(document).ready(function () {
                 var dynamicListElement = $('#dynamic-list');
                 dynamicListElement.empty();
                 dynamic_list.forEach(function (item) {
-                    dynamicListElement.append(makeParentCard(item));
+                    dynamicListElement.append(makeParentsCard(item));
                 });
             }
         });
     }
-    function makeParentCard(item) {
-        return '<li>'+
-        '<div class="big">'+
-        '<div class="small">'+item.male+'</div>'+
-        '<div class="small">'+item.female+'</div>'+
-        '</div>'+
-        '</li>';
+    function makeParentCard(name) {
+        return `
+        <div class="block min-w-32 text-left z-10">
+        <img class="inline size-12 bg-zinc-900 rounded-full border-2 border-zinc-800" src='./static/portrait/${name}.jpg'>
+        ${name}
+        </div>`;
+    }
+
+    function makeParentsCard(item) {
+        return `
+        <li class="max-w-[100%] w-128 mx-auto flex justify-between p-4 bg-[#343F45] rounded-[5px] shadow border-4 border-gray-800">
+            ${makeParentCard(item.male) + makeParentCard(item.female)}
+        </li>
+        `
     }
 
     // 초기 로드 시 동적 리스트 업데이트

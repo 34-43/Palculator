c3703d0b48f0349c09e506c7c63817dde3a54477
34-43 <121276581+34-43@users.noreply.github.com>
2024-01-31 02:43:07 +0900

    fix: tailwind js support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./api/templates/*.html"],
+  content: ["./api/templates/*.html","./api/templates/*/*.html","./api/static/js/main.js"],
   theme: {
     fontFamily: {
       'Pret': ['Pretendard']
